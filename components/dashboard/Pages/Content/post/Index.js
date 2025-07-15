b2b7da94c67d0f93ca07bd596fee92d87e972b97
melikaeslahi <melika.eslahi@gmail.com>
2025-07-15 23:05:53 +0330

    refactor(content/post):add TableHeaderComponent and delete toast logic

--- components/dashboard/Pages/Content/post/Index.js
+++ components/dashboard/Pages/Content/post/Index.js
@@ -1,19 +1,19 @@
 'use client'
-import TitlePage from "@/components/dashboard/TitlePage";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { modalOpenClose, setHandlerModal, setIsError, setIsLoading, setIsSuccess, setItemLength } from "@/store/reducers/dashboard/UtilSlice";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { Button } from "@/components/dashboard/inputs";
 import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Image from "next/image";
-import { toast } from "react-toastify";
 import { Table, TableContainer } from "@/components/dashboard/Table";
 import { useChangePostCommentableMutation, useChangePostStatusMutation, useDeletePostMutation, useGetAllPostQuery } from "@/lib/content/postApi";
 import Link from "next/link";
+import useToast from "@/hooks/useToast";
+import TableHeader from "@/components/dashboard/Table/TableHeader";
 const Index = () => {
-    const router = useRouter();
+  
     const dispatch = useDispatch();
     const pathname = usePathname();
     const { page, perPage, search } = useSelector((state) => state.util);
@@ -23,7 +23,7 @@ const Index = () => {
 
     const [changeStatus, { data: dataStatus }] =  useChangePostStatusMutation();
     const [changeCommentable, { data: dataCommentable }] =  useChangePostCommentableMutation();
-    const [deletePost, result] =  useDeletePostMutation();
+    const [deletePost, {result:deleteResult}] =  useDeletePostMutation();
 
     const handlerStatus = async (id) => {
         await changeStatus(id);
@@ -34,101 +34,48 @@ const Index = () => {
     }
 
     useEffect(()=>{
-        
         dispatch(setIsLoading(isLoading));
-        dispatch(setIsSuccess(isSuccess));
+} , [isLoading]);
+
+    useEffect(()=>{
+        dispatch(setIsSuccess(isSuccess)); 
+    } , [isSuccess]);
+
+    useEffect(()=>{
         dispatch(setIsError(isError));
+    } , [isError]);
+
+    useEffect(()=>{
         dispatch(setItemLength(posts.data?.length));
-  
+    } , [posts]);
 
-} , [isLoading , isSuccess , isError , posts])
 
-    useEffect(() => {
-        //result is response from useDeletePostCategoryMutation 
-        if (result.data) {
-            if (result.data.status === 200) {
-                toast.success('   پست با موفقیت حذف شد.', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                });
-            }
-        }
-
-    }, [result]);
 
     useEffect(() => {
-        // status checked and unchecked
-        if (dataStatus) {
+         useToast({result:deleteResult , message:'پست'})
+    }, [deleteResult]);
 
-            if (dataStatus.status === true && dataStatus.checked === true) {
-                toast.success('    پست  با موفقیت  فعال شد  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
-            } else if (dataStatus.status === true && dataStatus.checked === false) {
-
-                toast.success('   پست با موفقیت غیر فعال شد  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
-            } else if (dataStatus.status === false) {
-                toast.error('   خطایی پیش آمده است  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
-            }
-        }
-
+    useEffect(() => {
+        useToast({dataStatus:dataStatus , message:'پست'})
     }, [dataStatus])
 
     useEffect(() => {
-        //  commentable checked and unchecked
-        if (dataCommentable) {
-
-            if (dataCommentable.status === true && dataCommentable.checked === true) {
-                toast.success('      کامنت پست  با موفقیت  فعال شد  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
-            } else if (dataCommentable.status === true && dataCommentable.checked === false) {
-
-                toast.success(' کامنت پست   با موفقیت غیر فعال شد  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
-            } else if (dataCommentable.status === false) {
-                toast.error('   خطایی پیش آمده است  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
-            }
-        }
-
+         useToast({dataStatus:dataCommentable , message:'کامنت پست'})
     }, [dataCommentable])
 
 
     return (<>
-        <TitlePage
-            name='   پست ها  '
-            sitemapPage='بخش محتوایی  / پست ها '
-
-        >
-            <Link
-                href={`${pathname}/create`}
-                className="py-4 px-8 bg-pallete rounded text-white" >
-                {' '}
-                ایجاد  پست جدید
-            </Link>
-        </TitlePage>
-
+        <TableHeader 
+           title={'پست ها'}
+           href={`${pathname}/create`}
+           sitemap={'بخش محتوایی /پست ها'}
+        />
+        
         <TableContainer
-
             pagination={posts?.meta}
             deleteRecord={deletePost}
-
-
         >
-            {<Table  > 
+            {<Table > 
             <thead className="text-pallete  shadow-md">
                     <tr className={`text-center`}>
                      <th  className="pl-3 py-3"> # </th>
