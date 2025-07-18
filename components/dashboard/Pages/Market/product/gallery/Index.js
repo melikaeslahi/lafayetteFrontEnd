3702833193784fd9b28d4a26a4c5e5f2036bda60
melikaeslahi <melika.eslahi@gmail.com>
2025-07-18 22:24:55 +0330

    refactor(market/product/gallery):add TableHeaderComponent

--- components/dashboard/Pages/Market/product/gallery/Index.js
+++ components/dashboard/Pages/Market/product/gallery/Index.js
@@ -1,72 +1,49 @@
 'use client'
 import { Table, TableContainer } from "@/components/dashboard/Table";
-import TitlePage from "@/components/dashboard/TitlePage";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { modalOpenClose, setHandlerModal, setIsError, setIsLoading, setIsSuccess, setItemLength } from "@/store/reducers/dashboard/UtilSlice";
-import { usePathname, useRouter } from "next/navigation";
 import { Button } from "@/components/dashboard/inputs";
-import { faEdit, faEllipsis, faEllipsisV, faImage, faTrash } from "@fortawesome/free-solid-svg-icons"
+import { faTrash } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Image from "next/image";
-import { toast } from "react-toastify";
- 
-import Link from "next/link";
 import { useDeleteGalleryMutation, useGetAllGalleryQuery } from "@/lib/market/galleryApi";
+import useToast from "@/hooks/useToast";
+import TableHeader from "@/components/dashboard/Table/TableHeader";
+
 const Index = ({params}) => {
-    console.log(params)
     const dispatch = useDispatch();
-    const pathname = usePathname();
-  
-
     const { page, perPage  } = useSelector((state) => state.util);
-
-    // fetch post from localhost:8000/category?page=1,2,3
     const { data:  galleries = [], isError, isLoading, isSuccess } =  useGetAllGalleryQuery({ page, perPage , params});
-
-   
-
-    const [deleteImage, result] =  useDeleteGalleryMutation();
-
+    const [deleteImage, {result:deleteResult}] =  useDeleteGalleryMutation();
 
     useEffect(() => {
-
         dispatch(setIsLoading(isLoading));
-        dispatch(setIsSuccess(isSuccess));
-        dispatch(setIsError(isError));
-        dispatch(setItemLength(galleries.data?.length));
-
-    }, [isLoading, isSuccess, isError, galleries])
+    }, [isLoading]);
 
     useEffect(() => {
-        //result is response from useDeletePostCategoryMutation 
-        if (result.data) {
-            if (result.data.status === 200) {
-                toast.success(' محصول  با موفقیت حذف شد.', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                });
-            }
-        }
-    }, [result]);
-
+        dispatch(setIsSuccess(isSuccess));
+    }, [isSuccess]);
     
-
+    useEffect(() => {
+        dispatch(setIsError(isError));
+    }, [isError]);
     
-    return (<>
-        <TitlePage
-            name='گالری تصاویر'
-            sitemapPage='بخش فروش / ویترین / محصولات / گالری'  
+    useEffect(() => {
+        dispatch(setItemLength(galleries.data?.length));
+    }, [galleries]);
 
-        >
-            <Link
-                href={`/dashboard/market/product/gallery/create/${params}`}
-                className="py-4 px-8 bg-pallete rounded text-white" >
-                {' '}
-                ایجاد  تصویر  جدید
-            </Link>
-        </TitlePage>
-
+    useEffect(() => { 
+         useToast({result:deleteResult , message:'گالری'})
+    }, [deleteResult]);
+    
+    return (
+    <>
+    <TableHeader 
+     title={'گالری تصاویر'}
+     href={`/dashboard/market/product/gallery/create/${params}`}
+     sitemap={'بخش فروش / ویترین / محصولات / گالری'}
+    />
         <TableContainer
             pagination={galleries?.meta}
             deleteRecord={deleteImage}
@@ -86,24 +63,18 @@ const Index = ({params}) => {
                 return (
                     <> 
                     <tr key={index} className="text-center hover:bg-pallete hover:bg-opacity-20 hover:text-pallete  w-full  border-b-2 border-pallete">
-                        <td className="pl-3 py-3">{index+=1}</td>
-                       
+                        <td className="pl-3 py-3">{index+=1}</td>     
                         <td className="pl-3 py-3"   > {indexArray.map(([size, value]) => (
                             gallery.image.currentImage === size && <Image key={size} src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/${value}`} unoptimized={true} alt="image" className="w-12 h-12" width={'100'} height={'100'} />
                         ))}   </td>
-                        
                         <td className="pl-3 py-3">{gallery.product_id.name}</td>
-                       
-                        <td>
-                            
+                        <td> 
                             <Button type="button" onClick={() => {
                                 dispatch(setHandlerModal([gallery.product_id.name, gallery.id]))
                                 dispatch(modalOpenClose(true));
                             }} className="py-2 px-4 bg-red-500 hover:bg-red-600 rounded text-white">  <FontAwesomeIcon icon={faTrash} />     </Button>
                         </td>
-                    </tr>
-                    
-                  
+                    </tr>    
                      </>)
 
             })}
