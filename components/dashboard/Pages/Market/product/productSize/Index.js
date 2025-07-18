dc0b8d5502d9002c41d7261e9bac829194ce5945
melikaeslahi <melika.eslahi@gmail.com>
2025-07-18 22:39:07 +0330

    refactor(market/product/productSize):add TableHeaderComponent and delete toast logic

--- components/dashboard/Pages/Market/product/productSize/Index.js
+++ components/dashboard/Pages/Market/product/productSize/Index.js
@@ -2,74 +2,53 @@
 import { Table, TableContainer } from "@/components/dashboard/Table";
 import TitlePage from "@/components/dashboard/TitlePage";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect, useState } from "react";
+import { useEffect  } from "react";
 import { modalOpenClose, setHandlerModal, setIsError, setIsLoading, setIsSuccess, setItemLength } from "@/store/reducers/dashboard/UtilSlice";
-import { usePathname, useRouter } from "next/navigation";
 import { Button } from "@/components/dashboard/inputs";
-import { faEdit, faEllipsis, faEllipsisV, faImage, faTrash } from "@fortawesome/free-solid-svg-icons"
+import { faTrash } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import Image from "next/image";
-import { toast } from "react-toastify";
 import Link from "next/link";
 import { useDeleteProductSizeMutation, useGetAllProductSizeQuery } from "@/lib/market/productSizeApi";
+import useToast from "@/hooks/useToast";
+import TableHeader from "@/components/dashboard/Table/TableHeader";
 const Index = ({params}) => {
- 
     const dispatch = useDispatch();
-   
- 
-
     const { page, perPage, search } = useSelector((state) => state.util);
-
-    // fetch post from localhost:8000/category?page=1,2,3
     const { data:  productSizes = [], isLoading, isSuccess , isError  } =    useGetAllProductSizeQuery({ page, perPage, params , search  });
-
- 
-
-    const [deleteProductSize, result] =   useDeleteProductSizeMutation();
-
-    
-
-   
+    const [deleteProductSize, {result:deleteResult}] =   useDeleteProductSizeMutation();
 
     useEffect(() => {
-
         dispatch(setIsLoading(isLoading));
-        dispatch(setIsSuccess(isSuccess));
-        dispatch(setIsError(isError));
-        dispatch(setItemLength(productSizes.data?.length));
-
-    }, [isLoading, isSuccess, isError, productSizes])
+    }, [isLoading]);
 
     useEffect(() => {
-        //result is response from useDeletePostCategoryMutation 
-        if (result.data) {
-            if (result.data.status === 200) {
-                toast.success(' محصول  با موفقیت حذف شد.', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                });
-            }
-        }
-    }, [result]);
+        dispatch(setIsSuccess(isSuccess));
+    }, [isSuccess]);
+
+    useEffect(() => {
+        dispatch(setIsError(isError));
+    }, [isError]);
+
+    useEffect(() => {
+        dispatch(setItemLength(productSizes.data?.length));
+    }, [productSizes]);
+
+
+    useEffect(() => {
+         useToast({result:deleteResult , message:'سایز محصول'})
+    }, [deleteResult]);
 
     
 
  
 
     return (<>
-        <TitlePage
-            name=' سایز ها'
-            sitemapPage='    بخش فروش / ویترین / محصولات /سایز ها '
-
-        >
-            <Link
-                href={`/dashboard/market/product/productColor/create/${params}`}
-                className="py-4 px-8 bg-pallete rounded text-white" >
-                {' '}
-                ایجاد   سایز جدید
-            </Link>
-        </TitlePage>
-
+        <TableHeader  
+        title={' سایز ها'}
+        href={`/dashboard/market/product/productColor/create/${params}`}
+        sitemap={'بخش فروش / ویترین / محصولات /سایز ها '}
+        />
+    
         <TableContainer
             pagination={productSizes?.meta}
             deleteRecord={deleteProductSize}
@@ -96,21 +75,13 @@ const Index = ({params}) => {
                         <td className="pl-3 py-3">{productSize.size_name}</td>
                         <td className="pl-3 py-3">{productSize.size}</td>
                         <td className="pl-3 py-3">{productSize.price_increase ? productSize.price_increase : 'ندارد' }</td>
-
-
-                        <td>
-                            
+                        <td>                            
                             <Button type="button" onClick={() => {
                                 dispatch(setHandlerModal([productSize.product?.name, productSize.id]))
                                 dispatch(modalOpenClose(true));
                             }} className="py-2 px-4 bg-red-500 hover:bg-red-600 rounded text-white">  <FontAwesomeIcon icon={faTrash} />     </Button>
-                        </td>
-                       
-                            
-                     
-                    </tr>
-                    
-                  
+                        </td>                     
+                    </tr>                  
                      </>)
 
             })}
