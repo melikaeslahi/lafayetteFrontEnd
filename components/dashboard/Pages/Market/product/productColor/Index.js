9bdca001d0e63d0dfb40512bf7e892bdb4eb3e56
melikaeslahi <melika.eslahi@gmail.com>
2025-07-18 22:33:10 +0330

    refactor(market/product/productColor):add TableHeaderComponent and delete toast logic

--- components/dashboard/Pages/Market/product/productColor/Index.js
+++ components/dashboard/Pages/Market/product/productColor/Index.js
@@ -2,74 +2,54 @@
 import { Table, TableContainer } from "@/components/dashboard/Table";
 import TitlePage from "@/components/dashboard/TitlePage";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { modalOpenClose, setHandlerModal, setIsError, setIsLoading, setIsSuccess, setItemLength } from "@/store/reducers/dashboard/UtilSlice";
-import { usePathname, useRouter } from "next/navigation";
 import { Button } from "@/components/dashboard/inputs";
-import { faEdit, faEllipsis, faEllipsisV, faImage, faTrash } from "@fortawesome/free-solid-svg-icons"
+import { faTrash } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import Image from "next/image";
 import { toast } from "react-toastify";
- 
 import Link from "next/link";
 import { useDeleteProductColorMutation, useGetAllProductColorQuery } from "@/lib/market/productColorApi";
+import useToast from "@/hooks/useToast";
+import TableHeader from "@/components/dashboard/Table/TableHeader";
+
 const Index = ({params}) => {
- 
+
     const dispatch = useDispatch();
-   
- 
-
     const { page, perPage, search } = useSelector((state) => state.util);
-
-    // fetch post from localhost:8000/category?page=1,2,3
     const { data:  productColors = [], isLoading, isSuccess , isError  } =   useGetAllProductColorQuery({ page, perPage, params , search  });
-
- 
-
-    const [deleteProductColor, result] =  useDeleteProductColorMutation();
-
-    
-
-   
+    const [deleteProductColor, {result:deleteResult}] =  useDeleteProductColorMutation();
 
     useEffect(() => {
-
         dispatch(setIsLoading(isLoading));
+    }, [isLoading]);
+    
+    useEffect(() => {
         dispatch(setIsSuccess(isSuccess));
-        dispatch(setIsError(isError));
-        dispatch(setItemLength(productColors.data?.length));
-
-    }, [isLoading, isSuccess, isError, productColors])
+    }, [isSuccess]);
 
     useEffect(() => {
-        //result is response from useDeletePostCategoryMutation 
-        if (result.data) {
-            if (result.data.status === 200) {
-                toast.success(' محصول  با موفقیت حذف شد.', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                });
-            }
-        }
-    }, [result]);
+        dispatch(setIsError(isError));
+    }, [isError]);
+    useEffect(() => { 
+        dispatch(setItemLength(productColors.data?.length));
+    }, [productColors]);
+
+    useEffect(() => {
+        useToast({result:deleteResult , message:'رنگ محصول'})
+    }, [deleteResult]);
 
     
 
  
 
-    return (<>
-        <TitlePage
-            name=' رنگ ها'
-            sitemapPage='    بخش فروش / ویترین / محصولات /رنگ ها '
-
-        >
-            <Link
-                href={`/dashboard/market/product/productColor/create/${params}`}
-                className="py-4 px-8 bg-pallete rounded text-white" >
-                {' '}
-                ایجاد   رنگ جدید
-            </Link>
-        </TitlePage>
+    return (
+    <>
+        <TableHeader 
+        title={'رنگ ها'}
+        href={`/dashboard/market/product/productColor/create/${params}`}
+        sitemap={'بخش فروش / ویترین / محصولات /رنگ ها '}
+        />
 
         <TableContainer
             pagination={productColors?.meta}
@@ -97,23 +77,14 @@ const Index = ({params}) => {
                         <td className="pl-3 py-3">{productColor.color_name}</td>
                         <td className="pl-3 py-3">{productColor.color}</td>
                         <td className="pl-3 py-3">{productColor.price_increase ? productColor.price_increase : 'ندارد' }</td>
-
-
-                        <td>
-                            
+                        <td>            
                             <Button type="button" onClick={() => {
                                 dispatch(setHandlerModal([productColor.product?.name, productColor.id]))
                                 dispatch(modalOpenClose(true));
                             }} className="py-2 px-4 bg-red-500 hover:bg-red-600 rounded text-white">  <FontAwesomeIcon icon={faTrash} />     </Button>
                         </td>
-                       
-                            
-                     
-                    </tr>
-                    
-                  
+                    </tr>                  
                      </>)
-
             })}
                 </tbody>    
              </Table>}
