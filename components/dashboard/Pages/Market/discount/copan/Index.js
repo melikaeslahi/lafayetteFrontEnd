0c4f6a36f2ab8252cff4401a7737c29e0db0eff6
melikaeslahi <melika.eslahi@gmail.com>
2025-07-18 22:11:02 +0330

    refactor(market/discount/copan)

--- components/dashboard/Pages/Market/discount/copan/Index.js
+++ components/dashboard/Pages/Market/discount/copan/Index.js
@@ -1,71 +1,51 @@
 'use client'
 import { Table, TableContainer } from "@/components/dashboard/Table";
-import TitlePage from "@/components/dashboard/TitlePage";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { modalOpenClose, setHandlerModal, setIsError, setIsLoading, setIsSuccess, setItemLength } from "@/store/reducers/dashboard/UtilSlice";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { Button } from "@/components/dashboard/inputs";
 import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import Image from "next/image";
-import { toast } from "react-toastify";
 import Link from "next/link";
 import { useDeleteCopanMutation, useGetAllCopanQuery } from "@/lib/market/copanApi";
+import useToast from "@/hooks/useToast";
+import TableHeader from "@/components/dashboard/Table/TableHeader";
+
 const Index = () => {
-    const router = useRouter();
     const dispatch = useDispatch();
     const pathname = usePathname();
     const { page, perPage, search } = useSelector((state) => state.util);
-
-    // fetch post from localhost:8000/category?page=1,2,3
     const { data:  copans = [], isError, isLoading, isSuccess } =  useGetAllCopanQuery({ page, perPage, search });
-
-  
-
-    const [deleteCopan, result] =  useDeleteCopanMutation();
-
-  
+    const [deleteCopan, {result:deleteResult}] =  useDeleteCopanMutation();
 
     useEffect(() => {
-
         dispatch(setIsLoading(isLoading));
-        dispatch(setIsSuccess(isSuccess));
-        dispatch(setIsError(isError));
-        dispatch(setItemLength(copans.data?.length));
-
     }, [isLoading, isSuccess, isError, copans])
 
     useEffect(() => {
-        //result is response from useDeletePostCategoryMutation 
-        if (result.data) {
-            if (result.data.status === 200) {
-                toast.success('دسته بندی محصولات با موفقیت حذف شد.', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                });
-            }
-        }
-    }, [result]);
+        dispatch(setIsSuccess(isSuccess));
+    }, [isSuccess])
 
- 
+    useEffect(() => {
+        dispatch(setIsError(isError));
+    }, [isError]);
 
- 
+    useEffect(() => {
+        dispatch(setItemLength(copans.data?.length));
+    }, [copans]);
+
+    useEffect(() => {
+         useToast({result:deleteResult , message:'کپن تخفیف'});
+    }, [deleteResult]);
 
     return (<>
-        <TitlePage
-            name='کپن تخفیف'
-            sitemapPage='بخش فروش/ویترین/    تخفیف ها / کپن تخفیف'
-
-        >
-            <Link
-                href={`${pathname}/create`}
-                className="py-4 px-8 bg-pallete rounded text-white" >
-                {' '}
-                ایجاد  تخفیف جدید
-            </Link>
-        </TitlePage>
-
+    <TableHeader 
+    title={'کپن تخفیف'}
+    href={`${pathname}/create`}
+    sitemap={'بخش فروش/ویترین/ تخفیف ها / کپن تخفیف'}
+    />
+         
         <TableContainer
             pagination={copans?.meta}
             deleteRecord={deleteCopan}
