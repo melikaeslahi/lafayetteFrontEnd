6ec02d943a832ef839dfdf6b27c0d05d2f582eba
melikaeslahi <melika.eslahi@gmail.com>
2025-07-25 21:36:34 +0330

    refactor(market/comment):delete toast logic and add TableHeader component

--- components/dashboard/Pages/Market/comment/Index.js
+++ components/dashboard/Pages/Market/comment/Index.js
@@ -1,29 +1,24 @@
 'use client'
 import { Table, TableContainer } from "@/components/dashboard/Table";
-import TitlePage from "@/components/dashboard/TitlePage";
+ 
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { setIsError, setIsLoading, setIsSuccess, setItemLength } from "@/store/reducers/dashboard/UtilSlice";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { Button } from "@/components/dashboard/inputs";
- 
-import { toast } from "react-toastify";
- 
+
 import Link from "next/link";
 import { useChangeApprovedMutation, useChangeCommentStatusMutation, useGetAllCommentQuery } from "@/lib/market/commentApi";
+import TableHeader from "@/components/dashboard/Table/TableHeader";
+import useToast from "@/hooks/useToast";
+
 const Index = () => {
-    const router = useRouter();
     const dispatch = useDispatch();
     const pathname = usePathname();
     const { page, perPage, search } = useSelector((state) => state.util);
-
-    // fetch post from localhost:8000/category?page=1,2,3
     const { data: comments = [], isError, isLoading, isSuccess } = useGetAllCommentQuery({ page, perPage, search });
- 
     const [chengeStatus, { data: dataStatus }] =  useChangeCommentStatusMutation();
     const [chengeApproved, { data: dataApproved }] =  useChangeApprovedMutation();
-
- 
 
     const handlerStatus = async (id) => {
         await chengeStatus(id);
@@ -34,39 +29,24 @@ const Index = () => {
     }
 
     useEffect(() => {
-
         dispatch(setIsLoading(isLoading));
-        dispatch(setIsSuccess(isSuccess));
-        dispatch(setIsError(isError));
-        dispatch(setItemLength(comments.data?.length));
+    }, [isLoading]);
 
-    }, [isLoading, isSuccess, isError, comments])
+    useEffect(() => {
+        dispatch(setIsSuccess(isSuccess));
+    }, [isSuccess]);
+    useEffect(() => {
+        dispatch(setIsError(isError));
+    }, [isError]);
+
+    useEffect(() => {
+        dispatch(setItemLength(comments.data?.length));
+    }, [comments]);
 
    
 
     useEffect(() => {
-        // status checked and unchecked
-        if (dataStatus) {
-
-            if (dataStatus.status === true && dataStatus.checked === true) {
-                toast.success('کامنت   با موفقیت  فعال شد  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
-            } else if (dataStatus.status === true && dataStatus.checked === false) {
-
-                toast.success('       کامنت با موفقیت غیر فعال شد  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
-            } else if (dataStatus.status === false) {
-                toast.error('   خطایی پیش آمده است  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
-            }
-        }
-
+       useToast({dataStatus:dataStatus , message:'کامنت'});
     }, [dataStatus])
 
     useEffect(() => {
@@ -95,22 +75,14 @@ const Index = () => {
     }, [dataApproved])
 
     return (<>
-        <TitlePage
-            name=' کامنت   ها'
-            sitemapPage='بخش فروش/ویترین/  کامنت ها'
-
-        >
-            <Link
-                href={`${pathname}/create`}
-                className="py-4 px-8 bg-pallete rounded text-white" disabled>
-                {' '}
-                ایجاد کامنت
-            </Link>
-        </TitlePage>
-
+        <TableHeader 
+        title={'کامنت'}
+        sitemap={'بخش فروش/ویترین/ کامنت ها'}
+        href={`${pathname}/create`}
+        />
+         
         <TableContainer
             pagination={comments?.meta}
-       
         >
             {<Table> 
             <thead className="text-pallete  shadow-md">
@@ -128,15 +100,11 @@ const Index = () => {
                     </tr>
                 </thead>
                 <tbody>
-            {comments.data?.map((comment, index) => {
+            {comments.data?.map((comment) => {
              
                 return (
-                    <tr key={index} className="text-center hover:bg-pallete hover:bg-opacity-20 hover:text-pallete  w-full  border-b-2 border-pallete">
-                        <td className="pl-3 py-3">{index+=1}</td>
-                       
-                        
-                       
-                        
+                    <tr key={comment.id} className="text-center hover:bg-pallete hover:bg-opacity-20 hover:text-pallete  w-full  border-b-2 border-pallete">
+                        <td className="pl-3 py-3">{comment.id}</td> 
                         <td className="pl-3 py-3">{comment.body.replace(/<(.|\n)*?>/g, '').slice(0, 10)}</td>
                         <td className="pl-3 py-3">{comment.parent ? comment.parent.user.first_name + '' +comment.parent.user.last_name : ' '}</td>
                         <td className="pl-3 py-3">{comment.author_id  }</td>
@@ -144,15 +112,11 @@ const Index = () => {
                         <td className="pl-3 py-3">{comment.commentable.id  }</td>
                         <td className="pl-3 py-3">{comment.commentable?.name  }</td>
                         <td className="pl-3 py-3">{comment.approved == 1 ? 'تایید شده': 'تایید نشده'  }</td>
-
-
-
                         <td className="pl-3 py-3">
                             {<input type="checkbox" name="status" defaultChecked={comment.status === 1 ? true : false} onChange={() => handlerStatus(comment.id)} />}
                         </td>
-                       
                         <td>
-                            <Link href={`${pathname}/show/${comment.id}`} className="py-2 px-4 bg-green-500 hover:bg-green-600  rounded text-white">  نمایش     </Link>
+                            <Link href={`${pathname}/show/${comment.id}`} className="py-2 px-4 bg-green-500 hover:bg-green-600  rounded text-white">  نمایش </Link>
 
                             {comment.approved == 1 ? <Button type="button" onClick={() => {
                                  handlerApproved(comment.id)
