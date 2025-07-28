b5bf7f1e83b3d76c5a13ab3aacbd91aa9208e5a7
melikaeslahi <melika.eslahi@gmail.com>
2025-07-28 22:41:34 +0330

    refactor(content/market/order/returned):delete toast and query statu  handle logic and add TableHeader Component

--- components/dashboard/Pages/Market/orders/returned/Index.js
+++ components/dashboard/Pages/Market/orders/returned/Index.js
@@ -1,34 +1,27 @@
 'use client'
 import { Table, TableContainer } from "@/components/dashboard/Table";
-import TitlePage from "@/components/dashboard/TitlePage";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
-import { modalOpenClose, setHandlerModal, setIsError, setIsLoading, setIsSuccess, setItemLength } from "@/store/reducers/dashboard/UtilSlice";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { Button } from "@/components/dashboard/inputs";
-import { faEdit, faEllipsisV, faTrash } from "@fortawesome/free-solid-svg-icons"
+import { faEllipsisV } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import Image from "next/image";
-import { toast } from "react-toastify";
-
 import Link from "next/link";
 import { useCancelOrderMutation  , useChangeOrderStatusMutation, useChangeSendStatusMutation,   useGetReturnOrderQuery } from "@/lib/market/orderApi";
+import useToast from "@/hooks/useToast";
+import TableHeader from "@/components/dashboard/Table/TableHeader";
 const Index = () => {
-    const router = useRouter();
-    const dispatch = useDispatch();
     const pathname = usePathname();
     const [setting, setSetting] =  useState(false);
     const [settingId, setSettingId] = useState('');
     const { page, perPage, search } = useSelector((state) => state.util);
 
-    // fetch post from localhost:8000/category?page=1,2,3
-    const { data: orders = [], isError, isLoading, isSuccess } =   useGetReturnOrderQuery({ page, perPage, search });
+    const query =   useGetReturnOrderQuery({ page, perPage, search });
+    const orders= query?.id;
 
     const [chengeSendStatus, { data: dataSendStatus }] =   useChangeSendStatusMutation();
     const [changeOrderStatus, { data: dataOrderStatus }] =  useChangeOrderStatusMutation();
     const [cancelOrder, { data: dataCancelOrder }] = useCancelOrderMutation();
-
-
 
 
     const handlerSetting = (productId) => {
@@ -49,90 +42,45 @@ const Index = () => {
     }
 
     useEffect(() => {
-
-        dispatch(setIsLoading(isLoading));
-        dispatch(setIsSuccess(isSuccess));
-        dispatch(setIsError(isError));
-        dispatch(setItemLength(orders.data?.length));
-
-    }, [isLoading, isSuccess, isError, orders])
-
-
-
-    useEffect(() => {
-        // status checked and unchecked
-        if (dataSendStatus) {
-
-            if (dataSendStatus.status === 200) {
-                toast.success('         وضعیت  ارسال با موفقیت تغییر کرد       ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
-            } else if (dataSendStatus.status === 404) {
-                toast.error('   خطایی پیش آمده است  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
-            }
+        let message;
+        if (dataSendStatus?.status === 200) {
+             message=' وضعیت  ارسال با موفقیت تغییر کرد '
+        }else{
+            message= 'خطایی پیش آمده است '
         }
-
+        useToast({dataStatus:dataSendStatus , customMessage:message})
+    
     }, [dataSendStatus])
 
     useEffect(() => {
-        // status checked and unchecked
-        if (dataOrderStatus) {
-
-            if (dataOrderStatus.status === 200) {
-                toast.success(' وضعیت سفارش با موفقیت تغییر کرد  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
-            } else if (dataOrderStatus.status === 404) {
-                toast.error('   خطایی پیش آمده است  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
-            }
+        let message;
+        if (dataOrderStatus?.status === 200) {
+             message=' وضعیت  سفارش با موفقیت تغییر کرد '
+        }else{
+            message= 'خطایی پیش آمده است '
         }
-
+        useToast({dataStatus:dataOrderStatus , customMessage:message})
     }, [dataOrderStatus])
 
     useEffect(() => {
-        // status checked and unchecked
-        if (dataCancelOrder) {
-
-            if (dataCancelOrder.status === 200) {
-                toast.success('    شفارش با موفقیت باطل شد  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
-            } else if (dataCancelOrder.status === 404) {
-                toast.error('   خطایی پیش آمده است  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
-            }
+        let message;
+        if (dataCancelOrder?.status === 200) {
+             message=' شفارش با موفقیت باطل شد '
+        }else{
+            message= 'خطایی پیش آمده است '
         }
-
+        useToast({dataStatus:dataCancelOrder , customMessage:message})
     }, [dataCancelOrder])
 
     return (<>
-        <TitlePage
-            name='  سفارشات'
-            sitemapPage='بخش فروش/ویترین/ سفارشات'
-
-        >
-            <Link
-                href={`${pathname}/create`}
-                className="py-4 px-8 bg-pallete rounded text-white" >
-                {' '}
-                ایجاد دسته جدید
-            </Link>
-        </TitlePage>
-
+    <TableHeader 
+      title={'سفارشات'}
+      href={`${pathname}/create`}
+      sitemap={'بخش فروش/ویترین/ سفارشات'}
+      />
         <TableContainer
             pagination={orders?.meta}
-
+            query={query}
         >
             {<Table>
                 <thead className="text-pallete  shadow-md">
@@ -156,8 +104,8 @@ const Index = () => {
                     {orders.data?.map(( order, index) => {
                       
                         return (
-                            <tr key={index} className=" text-center hover:bg-pallete hover:bg-opacity-20 hover:text-pallete  w-full  border-b-2 border-pallete">
-                                    <td className="pl-3 py-3">{index += 1}</td>
+                            <tr key={order.id} className=" text-center hover:bg-pallete hover:bg-opacity-20 hover:text-pallete  w-full  border-b-2 border-pallete">
+                                    <td className="pl-3 py-3">{order.id}</td>
                                     <td className="pl-3 py-3"> { order.id } </td>
                                     <td className="pl-3 py-3"> { order.order_final_amount } تومان</td>
                                     <td className="pl-3 py-3"> { order.order_discount_amount } تومان</td>
