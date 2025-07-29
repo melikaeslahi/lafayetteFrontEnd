ed32906611fb0b32b480d039f5f90cd07c09f6ad
melikaeslahi <melika.eslahi@gmail.com>
2025-07-29 22:19:08 +0330

    refactor(content/market/product):delete toast and query statu  handle logic and add TableHeader Component

--- components/dashboard/Pages/Market/product/Index.js
+++ components/dashboard/Pages/Market/product/Index.js
@@ -1,19 +1,18 @@
 'use client'
 import { Table, TableContainer } from "@/components/dashboard/Table";
-import TitlePage from "@/components/dashboard/TitlePage";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
-import { modalOpenClose, setHandlerModal, setIsError, setIsLoading, setIsSuccess, setItemLength } from "@/store/reducers/dashboard/UtilSlice";
-import { usePathname, useRouter } from "next/navigation";
+import { modalOpenClose, setHandlerModal } from "@/store/reducers/dashboard/UtilSlice";
+import { usePathname } from "next/navigation";
 import { Button } from "@/components/dashboard/inputs";
-import { faEdit, faEllipsis, faEllipsisV, faImage, faTrash } from "@fortawesome/free-solid-svg-icons"
+import { faEdit, faEllipsisV, faImage, faTrash } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Image from "next/image";
-import { toast } from "react-toastify";
 import { useChangeProductStatusMutation, useChangeMarketableMutation, useDeleteProductMutation, useGetAllProductQuery } from "@/lib/market/productApi";
 import Link from "next/link";
+import useToast from "@/hooks/useToast";
+import TableHeader from "@/components/dashboard/Table/TableHeader";
 const Index = () => {
-    const router = useRouter();
     const dispatch = useDispatch();
     const pathname = usePathname();
     const [setting , setSetting] = useState(false);
@@ -21,8 +20,8 @@ const Index = () => {
 
     const { page, perPage, search } = useSelector((state) => state.util);
 
-    // fetch post from localhost:8000/category?page=1,2,3
-    const { data:  products = [], isError, isLoading, isSuccess } = useGetAllProductQuery({ page, perPage, search });
+    const  query = useGetAllProductQuery({ page, perPage, search });
+    const products = query?.data;
 
     const [chengeStatus, { data: dataStatus }] = useChangeProductStatusMutation();
     const [chengeMarketable, { data:  dataMarketable }] = useChangeMarketableMutation();
@@ -43,93 +42,27 @@ const Index = () => {
     }
 
     useEffect(() => {
-
-        dispatch(setIsLoading(isLoading));
-        dispatch(setIsSuccess(isSuccess));
-        dispatch(setIsError(isError));
-        dispatch(setItemLength(products.data?.length));
-
-    }, [isLoading, isSuccess, isError, products])
-
-    useEffect(() => {
-        //result is response from useDeletePostCategoryMutation 
-        if (result.data) {
-            if (result.data.status === 200) {
-                toast.success(' محصول  با موفقیت حذف شد.', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                });
-            }
-        }
+       useToast({result:result , message:'محصول'})
     }, [result]);
 
     useEffect(() => {
-        // status checked and unchecked
-        if (dataStatus) {
-
-            if (dataStatus.status === true && dataStatus.checked === true) {
-                toast.success('    محصول با موفقیت  فعال شد  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
-            } else if (dataStatus.status === true && dataStatus.checked === false) {
-
-                toast.success('      محصول با موفقیت غیر فعال شد  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
-            } else if (dataStatus.status === false) {
-                toast.error('   خطایی پیش آمده است  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
-            }
-        }
-
-    }, [dataStatus])
+        useToast({dataStatus:dataStatus , message:'محصول'})
+    }, [dataStatus]);
 
     useEffect(() => {
-        // status checked and unchecked
-        if (dataMarketable) {
-
-            if (dataMarketable.status === true && dataMarketable.checked === true) {
-                toast.success('          قابلیت فزوش  محصول با موفقیت  فعال شد  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
-            } else if (dataMarketable.status === true && dataMarketable.checked === false) {
-
-                toast.success(' قابلیت فروش  محصول با موفقیت غیر فعال شد ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
-            } else if (dataMarketable.status === false) {
-                toast.error('   خطایی پیش آمده است  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
-            }
-        }
-
+       useToast({dataStatus:dataMarketable , message:'قابلیت فروش محصول'})
     }, [dataMarketable])
 
     return (<>
-        <TitlePage
-            name='محصولات'
-            sitemapPage='بخش فروش/ویترین/ محصولات'
-
-        >
-            <Link
-                href={`${pathname}/create`}
-                className="py-4 px-8 bg-pallete rounded text-white" >
-                {' '}
-                ایجاد  محصول جدید
-            </Link>
-        </TitlePage>
-
+     <TableHeader 
+      title={'محصولات'}
+      sitemap={'بخش فروش/ویترین/ محصولات'}
+      href={`${pathname}/create`}
+     />
         <TableContainer
             pagination={products?.meta}
             deleteRecord={deleteProduct}
+            query={query}
         >
             {<Table> 
             <thead className="text-pallete  shadow-md">
@@ -148,12 +81,12 @@ const Index = () => {
                     </tr>
                 </thead>
                 <tbody >
-            {products.data?.map((product, index) => {
+            {products.data?.map((product) => {
                 const indexArray = Object.entries(product.image.indexArray);
                 return (
                     <> 
-                    <tr key={index} className="text-center hover:bg-pallete hover:bg-opacity-20 hover:text-pallete  w-full  border-b-2 border-pallete">
-                        <td className="pl-3 py-3">{index+=1}</td>
+                    <tr key={product.id} className="text-center hover:bg-pallete hover:bg-opacity-20 hover:text-pallete  w-full  border-b-2 border-pallete">
+                        <td className="pl-3 py-3">{product.id}</td>
                         <td className="pl-3 py-3">{product.name}</td>
                         <td className="pl-3 py-3"   > {indexArray.map(([size, value]) => (
                             product.image.currentImage === size && <Image key={size} src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/${value}`} unoptimized={true} alt="image" className="w-12 h-12" width={'100'} height={'100'} />
@@ -171,9 +104,9 @@ const Index = () => {
                         
                         <td className="pl-3 py-3">{product.slug}</td>
                         <td>
-                             <button onClick={()=>handlerSetting(product.id)} className="py-2 px-4   rounded ">  <FontAwesomeIcon icon={faEllipsisV} />     </button>
-                           
-                           
+                             <button onClick={()=>handlerSetting(product.id)} className="py-2 px-4   rounded ">  
+                             <FontAwesomeIcon icon={faEllipsisV} />
+                              </button>    
                         </td>
                         <section className={`${setting && settingId == product.id ? "absolute bg-white left-0 top-1 z-['12345'] flex flex-col w-32 h-auto shadow-md shadow-pallete" : 'hidden'}`}>
                              <Link href={`${pathname}/edit/${product.id}`} className="py-2 px-4   rounded text-right">  <FontAwesomeIcon icon={faEdit} />  ویرایش     </Link>
@@ -187,11 +120,8 @@ const Index = () => {
                                 dispatch(modalOpenClose(true));
                             }} className="py-2 px-4 rounded text-right">  <FontAwesomeIcon icon={faTrash} />  حذف   </Button>
                              </section>
-                    </tr>
-                    
-                  
+                    </tr>          
                      </>)
-
             })}
                 </tbody>    
              </Table>}
