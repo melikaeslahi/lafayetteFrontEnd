ff9ad6eeb79e69e3b94dfd229cad570f582dd3ba
melikaeslahi <melika.eslahi@gmail.com>
2025-07-29 22:42:24 +0330

    refactor(notify/sms):delete toast and query statu  handle logic and add TableHeader Component

--- components/dashboard/Pages/Notify/sms/Index.js
+++ components/dashboard/Pages/Notify/sms/Index.js
@@ -1,27 +1,23 @@
 'use client'
 import { Table, TableContainer } from "@/components/dashboard/Table";
-import TitlePage from "@/components/dashboard/TitlePage";
 import { useDispatch, useSelector } from "react-redux";
- 
 import { useEffect } from "react";
-import { modalOpenClose, setHandlerModal, setIsError, setIsLoading, setIsSuccess, setItemLength } from "@/store/reducers/dashboard/UtilSlice";
-import { usePathname, useRouter } from "next/navigation";
+import { modalOpenClose, setHandlerModal } from "@/store/reducers/dashboard/UtilSlice";
+import { usePathname } from "next/navigation";
 import { Button } from "@/components/dashboard/inputs";
 import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
- 
-import { toast } from "react-toastify";
- 
 import Link from "next/link";
 import { useChangeSMSStatusMutation, useDeleteSMSMutation, useGetAllSMSQuery } from "@/lib/notify/SMSApi";
+import useToast from "@/hooks/useToast";
+import TableHeader from "@/components/dashboard/Table/TableHeader";
 const Index = () => {
-    const router = useRouter();
     const dispatch = useDispatch();
     const pathname = usePathname();
     const { page, perPage, search } = useSelector((state) => state.util);
 
-    // fetch post from localhost:8000/category?page=1,2,3
-    const { data:  smses = [], isError, isLoading, isSuccess } =  useGetAllSMSQuery({ page, perPage, search });
+    const  query =  useGetAllSMSQuery({ page, perPage, search });
+    const smses = query?.data;
 
     const [chengeStatus, { data: dataStatus }] =   useChangeSMSStatusMutation();
     const [deleteSMS, result] =  useDeleteSMSMutation();
@@ -31,88 +27,41 @@ const Index = () => {
     }
 
     useEffect(() => {
-
-        dispatch(setIsLoading(isLoading));
-        dispatch(setIsSuccess(isSuccess));
-        dispatch(setIsError(isError));
-        dispatch(setItemLength(smses.data?.length));
-
-    }, [isLoading, isSuccess, isError, smses])
-
-    useEffect(() => {
-        //result is response from useDeletePostCategoryMutation 
-        if (result.data) {
-            if (result.data.status === 200) {
-                toast.success('   پیام با موفقیت حذف شد.', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                });
-            }
-        }
+        useToast({result: result , message:'پیام'})
     }, [result]);
 
     useEffect(() => {
-        // status checked and unchecked
-        if (dataStatus) {
-
-            if (dataStatus.status === true && dataStatus.checked === true) {
-                toast.success('     پیام  با موفقیت  فعال شد  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
-            } else if (dataStatus.status === true && dataStatus.checked === false) {
-
-                toast.success('     پیام با موفقیت غیر فعال شد  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
-            } else if (dataStatus.status === false) {
-                toast.error('   خطایی پیش آمده است  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
-            }
-        }
-
+       useToast({dataStatus:dataStatus , message:"پیام"})
     }, [dataStatus])
 
     return (<>
-        <TitlePage
-            name='   پیام ها'
-            sitemapPage=' بخش  اطلاع رسانی /    پیام ها  '
-
-        >
-            <Link
-                href={`${pathname}/create`}
-                className="py-4 px-8 bg-pallete rounded text-white" >
-                {' '}
-                ایجاد  پیام جدید
-            </Link>
-        </TitlePage>
-
+        <TableHeader 
+          title={'پیام ها'}
+          sitemap={' بخش  اطلاع رسانی / پیام ها  '}
+          href={`${pathname}/create`}
+        />
+    
         <TableContainer
             pagination={smses?.meta}
             deleteRecord={deleteSMS}
+            query={query}
         >
             {<Table>
                 <thead className="text-pallete  shadow-md">
                     <tr className={`text-center`}>
                         <th className="pl-3 py-3"> # </th>
                         <th className="pl-3 py-3">  عنوان </th>
-              
                         <th className="pl-3 py-3">  وضعیت   </th>
                         <th className="pl-3 py-3">  توضیحات </th>
-                        <th className="pl-3 py-3">          تاریخ انتشار    </th>
-                        
+                        <th className="pl-3 py-3"> تاریخ انتشار </th>       
                         <th className="pl-3 py-3">     تنظیمات   </th>
                     </tr>
                 </thead>
                 <tbody>
-                    {smses.data?.map((sms, index) => {
-             
+                    {smses.data?.map((sms) => { 
                         return (
-                            <tr key={index} className="text-center hover:bg-pallete hover:bg-opacity-20 hover:text-pallete  w-full  border-b-2 border-pallete">
-                                <td className="pl-3 py-3">{index+=1}</td>
+                            <tr key={sms.id} className="text-center hover:bg-pallete hover:bg-opacity-20 hover:text-pallete  w-full  border-b-2 border-pallete">
+                                <td className="pl-3 py-3">{sms.id}</td>
                                 <td className="pl-3 py-3">{sms.title}</td>
                                 
                                 <td className="pl-3 py-3">
@@ -120,7 +69,6 @@ const Index = () => {
                                 </td>
                                 <td className="pl-3 py-3">{sms.body.replace(/<(.|\n)*?>/g, '').slice(0, 10)}</td>
                                 <td className="pl-3 py-3">{sms.published_at}</td>
- 
                                 <td>
                                     <Link href={`${pathname}/edit/${sms.id}`} className="py-2 px-4 bg-green-500 hover:bg-green-600  rounded text-white">  <FontAwesomeIcon icon={faEdit} />     </Link>
                                     <Button type="button" onClick={() => {
