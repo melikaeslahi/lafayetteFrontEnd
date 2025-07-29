5ce4a4627f6027bda4928550d2f17662f7e5148f
melikaeslahi <melika.eslahi@gmail.com>
2025-07-29 22:25:16 +0330

    refactor(market/store):delete toast and query statu  handle logic and add TableHeader Component

--- components/dashboard/Pages/Market/store/Index.js
+++ components/dashboard/Pages/Market/store/Index.js
@@ -1,55 +1,30 @@
 'use client'
 import { Table, TableContainer } from "@/components/dashboard/Table";
-import TitlePage from "@/components/dashboard/TitlePage";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { modalOpenClose, setHandlerModal, setIsError, setIsLoading, setIsSuccess, setItemLength } from "@/store/reducers/dashboard/UtilSlice";
-import { usePathname, useRouter } from "next/navigation";
-import { Button } from "@/components/dashboard/inputs";
-import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons"
+import { useSelector } from "react-redux";
+import { usePathname  } from "next/navigation";
+import { faEdit  } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Image from "next/image";
-import { toast } from "react-toastify";
- 
 import Link from "next/link";
 import { useGetAllProductQuery } from "@/lib/market/storeApi";
-const Index = () => {
-   
-    const dispatch = useDispatch();
+import TableHeader from "@/components/dashboard/Table/TableHeader";
+const Index = () => {   
     const pathname = usePathname();
     const { page, perPage, search } = useSelector((state) => state.util);
 
-    // fetch post from localhost:8000/category?page=1,2,3
-    const { data:  products = [], isError, isLoading, isSuccess } =  useGetAllProductQuery({ page, perPage, search });
+    const query =  useGetAllProductQuery({ page, perPage, search });
+    const products = query?.data;
 
-    useEffect(() => {
-
-        dispatch(setIsLoading(isLoading));
-        dispatch(setIsSuccess(isSuccess));
-        dispatch(setIsError(isError));
-        dispatch(setItemLength(products.data?.length));
-
-    }, [isLoading, isSuccess, isError, products])
-
- 
 
     return (<>
-        <TitlePage
-            name='دسته بندی ها'
-            sitemapPage='بخش فروش/ویترین/دسته بندی ها'
-
-        >
-            <Link
-                href={`${pathname}/create`}
-                className="py-4 px-8 bg-pallete rounded text-white" disabled >
-                {' '}
-                ایجاد  انبار جدید
-            </Link>
-        </TitlePage>
-
+    <TableHeader 
+    title={'دسته بندی ها'}
+    sitemap={'بخش فروش/ویترین/دسته بندی ها'}
+    href={`${pathname}/create`}
+    />
         <TableContainer
             pagination={products?.meta}
-            
+            query={query}
         >
             {<Table> 
             <thead className="text-pallete  shadow-md">
@@ -59,31 +34,24 @@ const Index = () => {
                         <th className="pl-3 py-3">    تصویر   </th>
                         <th className="pl-3 py-3">   تعداد قابل فروش    </th>
                         <th className="pl-3 py-3">      تعداد فروخته شده     </th>
-                        <th className="pl-3 py-3">   تعداد رزرو شده </th>
-                      
+                        <th className="pl-3 py-3">   تعداد رزرو شده </th>               
                     </tr>
                 </thead>
                 <tbody>
-            {products.data?.map((product, index) => {
+            {products.data?.map((product) => {
                 const indexArray = Object.entries(product.image.indexArray);
                 return (
-                    <tr key={index} className="text-center hover:bg-pallete hover:bg-opacity-20 hover:text-pallete  w-full  border-b-2 border-pallete">
-                        <td className="pl-3 py-3">{index+=1}</td>
+                    <tr key={product.id} className="text-center hover:bg-pallete hover:bg-opacity-20 hover:text-pallete  w-full  border-b-2 border-pallete">
+                        <td className="pl-3 py-3">{product.id}</td>
                         <td className="pl-3 py-3">{product.name}</td>
                         <td className="pl-3 py-3"   > {indexArray.map(([size, value]) => (
                             product.image.currentImage === size && <Image key={size} src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/${value}`} unoptimized={true} alt="image" className="w-12 h-12" width={'100'} height={'100'} />
-                        ))}   </td>
-                        
-                        
-                        
+                        ))}   </td>                    
                         <td className="pl-3 py-3">{product.marketable_number}</td>
                         <td className="pl-3 py-3">{product.sold_number}</td>
-                        <td className="pl-3 py-3">{product.frozen_number}</td>
-
-                       
+                        <td className="pl-3 py-3">{product.frozen_number}</td>              
                         <td>
                         <Link href={`${pathname}/create/${product.id}`} className="py-2 px-4 bg-green-500 hover:bg-green-600  rounded text-white">   ایجاد      </Link>
-
                             <Link href={`${pathname}/edit/${product.id}`} className="py-2 px-4 bg-green-500 hover:bg-green-600  rounded text-white">  <FontAwesomeIcon icon={faEdit} />     </Link>
                              
                         </td>
