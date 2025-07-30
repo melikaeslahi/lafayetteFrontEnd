275c4606f261298bd5ed7a1d6a0658c3ee9442a5
melikaeslahi <melika.eslahi@gmail.com>
2025-07-30 23:34:16 +0330

    refactor(Ticket):delete toast and query statu  handle logic and add TableHeader Component

--- components/dashboard/Pages/Ticket/admin/Index.js
+++ components/dashboard/Pages/Ticket/admin/Index.js
@@ -1,93 +1,44 @@
 'use client'
 import { Table, TableContainer } from "@/components/dashboard/Table";
-import TitlePage from "@/components/dashboard/TitlePage";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useEffect } from "react";
-import { setIsError, setIsLoading, setIsSuccess, setItemLength } from "@/store/reducers/dashboard/UtilSlice";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { Button } from "@/components/dashboard/inputs";
 import {  faTrash } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
- 
-import { toast } from "react-toastify";
- 
-import Link from "next/link";
 import { useChangeTicketAdminStatusMutation, useGetAllTicketAdminQuery } from "@/lib/ticket/ticketAdminApi";
+import useToast from "@/hooks/useToast";
+import TableHeader from "@/components/dashboard/Table/TableHeader";
 const Index = () => {
-    const router = useRouter();
-    const dispatch = useDispatch();
     const pathname = usePathname();
     const { page, perPage, search } = useSelector((state) => state.util);
 
-    // fetch post from localhost:8000/category?page=1,2,3
-    const { data:  admins = [], isError, isLoading, isSuccess } =  useGetAllTicketAdminQuery({ page, perPage, search });
-
+    const  query =  useGetAllTicketAdminQuery({ page, perPage, search });
+    const admins = query?.data;
     const [chengeStatus, { data: dataStatus }] = useChangeTicketAdminStatusMutation();
-     
-
-    
-
+       
     const handlerStatus = async (id) => {
         await chengeStatus(id);
     }
-
-    
-    useEffect(() => {
-
-        dispatch(setIsLoading(isLoading));
-        dispatch(setIsSuccess(isSuccess));
-        dispatch(setIsError(isError));
-        dispatch(setItemLength(admins.data?.length));
-
-    }, [isLoading, isSuccess, isError, admins])
-
  
-
     useEffect(() => {
-        // status checked and unchecked
-        if (dataStatus) {
-
-            if (dataStatus.status === 200) {
-                toast.success('            تیکت ادمین تغییر کرد    ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
+           let message;
+            if (dataStatus?.status === 200) {
+                 message=' تیکت ادمین تغییر کرد '          
             }
-            //  else if (dataStatus.status === true && dataStatus.checked === false) {
-
-            //     toast.success('  دسته بندی محصولات با موفقیت غیر فعال شد  ', {
-            //         position: toast.POSITION.TOP_LEFT,
-            //         rtl: true
-            //     })
-            // } else if (dataStatus.status === false) {
-            //     toast.error('   خطایی پیش آمده است  ', {
-            //         position: toast.POSITION.TOP_LEFT,
-            //         rtl: true
-            //     })
-            // }
-        }
-
+            useToast({dataStatus: dataStatus , customMessage:message})
     }, [dataStatus])
 
-    
-
     return (<>
-        <TitlePage
-            name='  تیکت ادمین'
-            sitemapPage='بخش  تیکت ها/  تیکت ادمین'
-
-        >
-            <Link
-                href={`${pathname}/create`}
-                className="py-4 px-8 bg-pallete rounded text-white" >
-                {' '}
-                ایجاد  تیکت ادمین جدید
-            </Link>
-        </TitlePage>
-
+        <TableHeader 
+         title={'  تیکت ادمین'}
+         href={`${pathname}/create`}
+         sitemap={'بخش  تیکت ها/  تیکت ادمین'}
+        />
+         
         <TableContainer
             pagination={ admins?.meta}
-         
+            query={query}
         >
             {<Table> 
             <thead className="text-pallete  shadow-md">
@@ -98,18 +49,13 @@ const Index = () => {
                     </tr>
                 </thead>
                 <tbody>
-            { admins.data?.map(( admin, index) => {
- 
+            { admins.data?.map((admin) => {
                 return (
-                    <tr key={index} className="text-center hover:bg-pallete hover:bg-opacity-20 hover:text-pallete  w-full  border-b-2 border-pallete">
-                        <td className="pl-3 py-3">{index+=1}</td>
-                        <td className="pl-3 py-3">{admin.first_name + ' ' + admin.last_name } </td>
-                        
-                        
+                    <tr key={admin.id} className="text-center hover:bg-pallete hover:bg-opacity-20 hover:text-pallete  w-full  border-b-2 border-pallete">
+                        <td className="pl-3 py-3">{admin.id}</td>
+                        <td className="pl-3 py-3">{admin.first_name} {admin.last_name } </td>                  
                         <td className="pl-3 py-3">{admin.email}</td>
-                        
-                        <td>
-                          
+                        <td>  
                             <Button type="button" onClick={() => {
                                handlerStatus(admin.id)
                             }} className={`py-2 px-4 ${ admin.admin == null ? 'bg-green-500 hover:bg-green-600' :  `bg-red-500 hover:bg-red-600`} rounded text-white`}>  <FontAwesomeIcon icon={faTrash} />     </Button>
--- components/dashboard/Pages/Ticket/open/Index.js
+++ components/dashboard/Pages/Ticket/open/Index.js
@@ -1,90 +1,50 @@
 'use client'
 import { Table, TableContainer } from "@/components/dashboard/Table";
-import TitlePage from "@/components/dashboard/TitlePage";
-import { useDispatch, useSelector } from "react-redux";
+import {  useSelector } from "react-redux";
 import { useEffect } from "react";
-import { setIsError, setIsLoading, setIsSuccess, setItemLength } from "@/store/reducers/dashboard/UtilSlice";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { Button } from "@/components/dashboard/inputs";
-
-import { toast } from "react-toastify";
-
 import Link from "next/link";
 import { useChangeMutation,  useGetOpenTicketsQuery } from "@/lib/ticket/ticketApi";
+import useToast from "@/hooks/useToast";
+import TableHeader from "@/components/dashboard/Table/TableHeader";
  
 const Index = () => {
-    const router = useRouter();
-    const dispatch = useDispatch();
     const pathname = usePathname();
     const { page, perPage, search } = useSelector((state) => state.util);
 
-    // fetch post from localhost:8000/category?page=1,2,3
-    const { data:  tickets = [], isError, isLoading, isSuccess } =   useGetOpenTicketsQuery({ page, perPage, search });
+    const  query =   useGetOpenTicketsQuery({ page, perPage, search });
+    const tickets = query?.data;
 
     const [chenge, { data: dataStatus }] =  useChangeMutation();
  
-
-
-
     const handlerChange = async (id) => {
         await chenge(id);
     }
 
-    
     useEffect(() => {
-
-        dispatch(setIsLoading(isLoading));
-        dispatch(setIsSuccess(isSuccess));
-        dispatch(setIsError(isError));
-        dispatch(setItemLength(tickets.data?.length));
-
-    }, [isLoading, isSuccess, isError, tickets])
-
-
-
-    useEffect(() => {
-        // status checked and unchecked
-        if (dataStatus) {
-
+        let message;
             if (dataStatus.status === true && dataStatus.checked === true) {
-                toast.success(' تیکت با موفقیت باز شد  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
+               message=' تیکت با موفقیت باز شد  '             
             } else if (dataStatus.status === true && dataStatus.checked === false) {
-
-                toast.success('         با موفقیت    بسته  شد  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
+                message=' با موفقیت بسته  شد  ' 
             } else if (dataStatus.status === false) {
-                toast.error('   خطایی پیش آمده است  ', {
-                    position: toast.POSITION.TOP_LEFT,
-                    rtl: true
-                })
+                message='   خطایی پیش آمده است  '     
             }
-        }
+            useToast({dataStatus:dataStatus , customMessage: message})
 
     }, [dataStatus])
 
-   
     return (<>
-        <TitlePage
-            name=' تیکت ها '
-            sitemapPage='بخش فروش/ویترین/   تیکت ها'
-
-        >
-            <Link
-                href={`${pathname}/create`}
-                className="py-4 px-8 bg-pallete rounded text-white" disabled>
-                {' '}
-                ایجاد کامنت
-            </Link>
-        </TitlePage>
-
+      <TableHeader 
+        title={' تیکت ها '}
+        sitemap={'بخش فروش/ویترین/   تیکت ها'}
+        href={`${pathname}/create`}
+      />
+         
         <TableContainer
             pagination={tickets?.meta}
-
+            query={query}
         >
             {<Table>
                 <thead className="text-pallete  shadow-md">
@@ -101,15 +61,10 @@ const Index = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {tickets.data?.map((ticket, index) => {
-
+                    {tickets.data?.map((ticket) => {
                         return (
-                            <tr key={index} className="text-center hover:bg-pallete hover:bg-opacity-20 hover:text-pallete  w-full  border-b-2 border-pallete">
-                                <td className="pl-3 py-3">{index += 1}</td>
-
-
-
-
+                            <tr key={ticket.id} className="text-center hover:bg-pallete hover:bg-opacity-20 hover:text-pallete  w-full  border-b-2 border-pallete">
+                                <td className="pl-3 py-3">{ticket.id}</td>
                                 <td className="pl-3 py-3">{ticket.user.first_name + '' + ticket.user.last_name }</td>
                                 <td className="pl-3 py-3">{ticket.subject}</td>
                                 <td className="pl-3 py-3">{ticket.category.name}</td>
@@ -117,17 +72,12 @@ const Index = () => {
                                 <td className="pl-3 py-3">{ticket.admin ? ticket.admin.admin.first_name + ' ' + ticket.admin.admin.last_name : 'نامشخص' }</td>
                                 <td className="pl-3 py-3">{ticket.parent?.subject ?? '_' }</td>
                                 <td className="pl-3 py-3">{ticket.status == 1 ? 'بسته' : 'باز' }</td>
-
-                        
-
                                 <td>
                                     <Link href={`${pathname}/show/${ticket.id}`} className="py-2 px-4 bg-green-500 hover:bg-green-600  rounded text-white">  نمایش     </Link>
 
                                     <Button type="button" onClick={() => {
                                          handlerChange(ticket.id)
                                     }} className={`py-2 px-4   ${ticket.status == 1 ? ' bg-red-500 hover:bg-red-600' : 'bg-green-500 hover:bg-green-600' }  rounded text-white`}>  { ticket.status == 1 ?     'باز کردن' : 'بستن'   }  </Button> 
-                                        
-
                                 </td>
                             </tr>)
                     })}
