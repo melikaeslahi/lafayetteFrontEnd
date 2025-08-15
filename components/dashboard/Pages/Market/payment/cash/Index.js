d035e8f3854aaec4aed99994a6c504a42577e118
melikaeslahi <melika.eslahi@gmail.com>
2025-08-15 23:25:05 +0330

    refactor(market):add dynamic table component

--- components/dashboard/Pages/Market/payment/cash/Index.js
+++ components/dashboard/Pages/Market/payment/cash/Index.js
@@ -1,16 +1,13 @@
 'use client'
-import { Table, TableContainer } from "@/components/dashboard/Table";
-import { useDispatch, useSelector } from "react-redux";
+import { CustomTable } from "@/components/dashboard/Table";
+import {  useSelector } from "react-redux";
 import { useEffect } from "react"; 
 import { usePathname } from "next/navigation";
 import { Button } from "@/components/dashboard/inputs"; 
 import Link from "next/link";
 import { useCanceledPaymentMutation, useGetCashPaymentQuery, useReturnedPaymentMutation  } from "@/lib/market/paymentApi";
-import TableHeader from "@/components/dashboard/Table/TableHeader";
 import useToast from "@/hooks/useToast";
 const Index = () => {
-    
-    const dispatch = useDispatch();
     const pathname = usePathname();
     const { page, perPage, search } = useSelector((state) => state.util);
  
@@ -20,11 +17,28 @@ const Index = () => {
     const [ canceled , {data:dataCanceled}] =   useCanceledPaymentMutation( );
     const  [returned , {data:dataReturned}] =    useReturnedPaymentMutation( );
 
-    const handlerCanceled =   (id) => {
+    const  columns =[
+        {key:'payments', label:' کد تراکنش ', render:(value)=>value.transaction_id},
+        {key:'payments' , label:'بانک' , render:(value)=> value.gateway }, 
+        {key:'user_id' ,label:'پرداخت کننده ', render:(value)=> `${value.user_id.first_name} ${value.user_id.last_name}` } , 
+        {key:'status' ,label:' وضعیت پرداخت ' ,render:(value)=> value == 0 ? 'پرداخت نشده'  : value == 1 ? 'پرداخت شده' :  value == 2 ? 'باطل شده': 'برگشت داده شده' },
+        {key:'type' ,label:'نوع پرداخت' , render:(value)=>value == 0 ? ' آنلاین'  :  value == 1 ? 'آفلاین' :  'در محل'},
+        {key:'setting' , label:'تنظیمات', render:(_,row)=><>
+          <Link href={`${pathname}/show/${row.id}`} className="py-2 px-4 bg-green-500 hover:bg-green-600  rounded text-white"> نمایش </Link>
+            <Button type="button" onClick={() => {handlerCanceled(row.id) }}
+             className={`py-2 px-4 bg-red-500 hover:bg-red-600 rounded text-white  `}
+              disabled={row.status == 2 ? true : false} > باطل کردن </Button>
+             <Button type="button" onClick={() => {handlerReturned(row.id)}}
+             className={`py-2 px-4 bg-yellow-500 hover:bg-yellow-600 rounded text-white  `} 
+             disabled={row.status == 3 ? true : false}> برگشت دادن </Button>
+        </>}
+      ]
+
+    const handlerCanceled = (id) => {
         canceled(id);
     }
 
-    const handlerReturned =   (id) => {
+    const handlerReturned = (id) => {
         returned(id);
     }
 
@@ -49,53 +63,13 @@ const Index = () => {
     }, [dataReturned]);
 
 
-    return (<>
-    <TableHeader
-    title={'پرداخت ها'}
-    sitemap={'بخش فروش/ویترین/  پرداخت  ها'}
-    href={`${pathname}/create`}
-    />
-        <TableContainer
-            pagination={payments?.meta}
-            query={query}     
-        >
-            {<Table> 
-            <thead className="text-pallete  shadow-md">
-                    <tr className={`text-center`}>
-                        <th className="pl-3 py-3"> # </th>
-                        <th className="pl-3 py-3">     کد تراکنش </th>
-                        <th className="pl-3 py-3">     بانک   </th>
-                        <th className="pl-3 py-3">   پرداخت کننده   </th>
-                        <th className="pl-3 py-3">      وضعیت پرداخت     </th>
-                        <th className="pl-3 py-3">   نوع پرداخت </th>        
-                        <th className="pl-3 py-3">     تنظیمات   </th>
-                    </tr>
-                </thead>
-                <tbody>
-            { payments.data?.map((payment) => {  
-                return (
-                    <tr key={payment.id} className="text-center hover:bg-pallete hover:bg-opacity-20 hover:text-pallete  w-full  border-b-2 border-pallete">
-                        <td className="pl-3 py-3">{payment.id}</td>
-                        <td className="pl-3 py-3">{payment.payments?.transaction_id}</td>
-                        <td className="pl-3 py-3">{payment.payments?.gateway}</td>
-                        <td className="pl-3 py-3">{payment.user_id.first_name + ' ' + payment.user_id.last_name }</td>
-                        <td className="pl-3 py-3">{payment.status == 0 ? 'پرداخت نشده'  : payment.status == 1 ? 'پرداخت شده' : payment.status == 2 ? 'باطل شده': 'برگشت داده شده'} </td>
-                        <td className="pl-3 py-3">{payment.type == 0 ? ' آنلاین'  : payment.type == 1 ? ' آفلاین  ' :  '   در محل  '} </td>         
-                        <td>
-                            <Link href={`${pathname}/show/${payment.id}`} className="py-2 px-4 bg-green-500 hover:bg-green-600  rounded text-white">   نمایش      </Link>
-                            <Button type="button" onClick={() => {
-                                handlerCanceled(payment.id)      
-                            }} className={`py-2 px-4 bg-red-500 hover:bg-red-600 rounded text-white  `} disabled={payment.status == 2 ? true : false} >   باطل کردن    </Button>
-                             <Button type="button" onClick={() => {
-                             handlerReturned(payment.id)
-                          }} className={`py-2 px-4 bg-yellow-500 hover:bg-yellow-600 rounded text-white  `} disabled={payment.status == 3 ? true : false}>    برگشت دادن      </Button>
-                        </td>
-                    </tr>)
-            })}
-                </tbody>    
-             </Table>}
-        </TableContainer>
-    </>
+    return (
+     <CustomTable 
+        title={'پرداخت ها'}
+        sitemap={'بخش فروش/ویترین/  پرداخت  ها'}
+        pagination={payments?.meta}
+        data={query} 
+        columns={columns} /> 
     )
 }
 export default Index;
--- components/dashboard/Pages/Market/payment/online/Index.js
+++ components/dashboard/Pages/Market/payment/online/Index.js
@@ -1,13 +1,13 @@
 'use client'
-import { Table, TableContainer } from "@/components/dashboard/Table";
+import { CustomTable } from "@/components/dashboard/Table";
 import { useSelector } from "react-redux";
 import { useEffect  } from "react";
 import { usePathname } from "next/navigation";
 import { Button } from "@/components/dashboard/inputs";
 import Link from "next/link";
-import {   useCanceledPaymentMutation, useGetOnlinePaymentQuery,  useReturnedPaymentMutation  } from "@/lib/market/paymentApi";
-import TableHeader from "@/components/dashboard/Table/TableHeader";
+import { useCanceledPaymentMutation, useGetOnlinePaymentQuery,  useReturnedPaymentMutation  } from "@/lib/market/paymentApi";
 import useToast from "@/hooks/useToast";
+
 const Index = () => {
     const pathname = usePathname();
     const { page, perPage, search } = useSelector((state) => state.util);
@@ -15,23 +15,40 @@ const Index = () => {
     const  query=   useGetOnlinePaymentQuery({ page, perPage, search });
     const payments = query?.data;
 
-    const [ canceled , {data:dataCanceled}] =   useCanceledPaymentMutation( );
-    const  [returned , {data:dataReturned}] =    useReturnedPaymentMutation( );
+    const [canceled , {data:dataCanceled}] =  useCanceledPaymentMutation();
+    const [returned , {data:dataReturned}] =  useReturnedPaymentMutation();
 
-    const handlerCanceled =   (id) => {
+    const handlerCanceled = (id) => {
         canceled(id);
     }
 
-    const handlerReturned =   (id) => {
+    const handlerReturned = (id) => {
         returned(id);
     }
+
+    const  columns =[
+        {key:'payments', label:' کد تراکنش ', render:(value)=>value.transaction_id},
+        {key:'payments' , label:'بانک' , render:(value)=> value.gateway }, 
+        {key:'user_id' ,label:'پرداخت کننده ', render:(value)=> `${value.user_id.first_name} ${value.user_id.last_name}` } , 
+        {key:'status' ,label:' وضعیت پرداخت ' ,render:(value)=> value == 0 ? 'پرداخت نشده'  : value == 1 ? 'پرداخت شده' :  value == 2 ? 'باطل شده': 'برگشت داده شده' },
+        {key:'type' ,label:'نوع پرداخت' , render:(value)=>value == 0 ? ' آنلاین'  :  value == 1 ? 'آفلاین' :  'در محل'},
+        {key:'setting' , label:'تنظیمات', render:(_,row)=><>
+          <Link href={`${pathname}/show/${row.id}`} className="py-2 px-4 bg-green-500 hover:bg-green-600  rounded text-white"> نمایش </Link>
+            <Button type="button" onClick={() => {handlerCanceled(row.id) }}
+             className={`py-2 px-4 bg-red-500 hover:bg-red-600 rounded text-white  `}
+              disabled={row.status == 2 ? true : false} > باطل کردن </Button>
+             <Button type="button" onClick={() => {handlerReturned(row.id)}}
+             className={`py-2 px-4 bg-yellow-500 hover:bg-yellow-600 rounded text-white  `} 
+             disabled={row.status == 3 ? true : false}> برگشت دادن </Button>
+        </>}
+      ]
 
     useEffect(() => {
         let message;   
             if (dataCanceled?.status === 200) {
-               message =' پرداخت با  با موفقیت  باطل شد ' 
+               message =' پرداخت با موفقیت  باطل شد ' 
             }   else {  
-               message=' خطایی پیش آمده است  ' 
+               message=' خطایی پیش آمده است' 
             }
             useToast({dataStatus:dataCanceled , customMessage:message});
     }, [dataCanceled ])
@@ -47,54 +64,13 @@ const Index = () => {
     }, [dataReturned])
 
  
-    return (<>
-       <TableHeader 
+    return (
+      <CustomTable 
         title={'پرداخت ها'}
-        sitemap={'بخش فروش/ویترین/  پرداخت  ها'}
-        href={`${pathname}/create`}
-       />
-        <TableContainer
-            pagination={payments?.meta}
-            query={query}
-        >
-            {<Table> 
-            <thead className="text-pallete  shadow-md">
-                    <tr className={`text-center`}>
-                        <th className="pl-3 py-3"> # </th>
-                        <th className="pl-3 py-3">     کد تراکنش </th>
-                        <th className="pl-3 py-3">     بانک   </th>
-                        <th className="pl-3 py-3">   پرداخت کننده   </th>
-                        <th className="pl-3 py-3">      وضعیت پرداخت     </th>
-                        <th className="pl-3 py-3">   نوع پرداخت </th>
-                         
-                        <th className="pl-3 py-3">     تنظیمات   </th>
-                    </tr>
-                </thead>
-                <tbody>
-            { payments.data?.map((payment) => {
-                return (
-                    <tr key={payment} className="text-center hover:bg-pallete hover:bg-opacity-20 hover:text-pallete  w-full  border-b-2 border-pallete">
-                        <td className="pl-3 py-3">{payment}</td>
-                        <td className="pl-3 py-3">{payment.payments?.transaction_id}</td>
-                        <td className="pl-3 py-3">{payment.payments?.gateway}</td>
-                        <td className="pl-3 py-3">{payment.user_id.first_name + ' ' + payment.user_id.last_name }</td>
-                        <td className="pl-3 py-3">{payment.status == 0 ? 'پرداخت نشده'  : payment.status == 1 ? 'پرداخت شده' : payment.status == 2 ? 'باطل شده': 'برگشت داده شده'} </td>
-                        <td className="pl-3 py-3">{payment.type == 0 ? ' آنلاین'  : payment.type == 1 ? ' آفلاین  ' :  '   در محل  '} </td>     
-                        <td>
-                            <Link href={`${pathname}/show/${payment.id}`} className="py-2 px-4 bg-green-500 hover:bg-green-600  rounded text-white">   نمایش      </Link>
-                            <Button type="button" onClick={() => {
-                                handlerCanceled(payment.id)      
-                            }} className={`py-2 px-4 bg-red-500 hover:bg-red-600 rounded text-white  `} disabled={payment.status == 2 ? true : false} >   باطل کردن    </Button>
-                             <Button type="button" onClick={() => {
-                             handlerReturned(payment.id)
-                          }} className={`py-2 px-4 bg-yellow-500 hover:bg-yellow-600 rounded text-white  `} disabled={payment.status == 3 ? true : false}>    برگشت دادن      </Button>
-                        </td>
-                    </tr>)
-            })}
-                </tbody>    
-             </Table>}
-        </TableContainer>
-    </>
+        sitemap={'بخش فروش/ویترین/پرداخت ها'}
+        pagination={payments?.meta}
+        data={query}
+        columns={columns} /> 
     )
 }
 export default Index;
