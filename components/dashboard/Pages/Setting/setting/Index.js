47b0d16456ad067fdf4729ef5f7868f1f6a097e2
melikaeslahi <melika.eslahi@gmail.com>
2025-08-17 20:39:29 +0330

    refactor(setting):add dynamic table component

--- components/dashboard/Pages/Setting/setting/Index.js
+++ components/dashboard/Pages/Setting/setting/Index.js
@@ -1,71 +1,30 @@
 'use client'
-import { Table, TableContainer } from "@/components/dashboard/Table";
-import TitlePage from "@/components/dashboard/TitlePage";
+import { CustomTable, SettingRecord, ShowImage} from "@/components/dashboard/Table";
 import { useSelector } from "react-redux";
-import { usePathname } from "next/navigation";
-import { faEdit } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import Image from "next/image";
-import Link from "next/link";
 import { useGetAllSettingQuery } from "@/lib/setting/settingApi";
-import TableHeader from "@/components/dashboard/Table/TableHeader";
 const Index = () => {
-    const pathname = usePathname();
+ 
     const { page, perPage, search } = useSelector((state) => state.util);
 
     const  query =   useGetAllSettingQuery({ page, perPage, search });
     const settings = query?.data;
+    
+    const  columns =[
+        {key:'title', label:'عنوان'},
+        {key:'logo' , label:'لوگو' , render:(value)=><ShowImage image={value} />}, 
+        {key:'icon' , label:'آیکون' , render:(value)=><ShowImage image={value} />}, 
+        {key:'keywords' ,label:'کلمات کلیدی' } , 
+        {key:'description' ,label:'توضیحات' ,render:(value)=>value.replace(/<(.|\n)*?>/g, '').slice(0, 10) },
+        {key:'setting' , label:'تنظیمات', render:(_,row)=><SettingRecord id={row.id} />}
+      ]
 
-
-    return (<>
-        <TableHeader 
-         title={'تنظیمات'}
-         sitemap={'  تنظیمات'}
-         href={`${pathname}/create`}
-        />
-       
-        <TableContainer
-            pagination={settings?.meta}
-            query={query}
-        >
-            {<Table> 
-            <thead className="text-pallete  shadow-md">
-                    <tr className={`text-center`}>
-                        <th className="pl-3 py-3"> # </th>
-                        <th className="pl-3 py-3">     عنوان </th>
-                        <th className="pl-3 py-3">     لوگو   </th>
-                        <th className="pl-3 py-3">   آیکون   </th>
-                        <th className="pl-3 py-3">     کلمات کلیدی</th>
-                        <th className="pl-3 py-3">  توضیحات </th>
-                        <th className="pl-3 py-3">     تنظیمات   </th>
-                    </tr>
-                </thead>
-                <tbody>
-            {settings.data?.map((setting) => {
-                return (
-                    <tr key={setting.id} className="text-center hover:bg-pallete hover:bg-opacity-20 hover:text-pallete  w-full  border-b-2 border-pallete">
-                        <td className="pl-3 py-3">{setting.id}</td>
-                        <td className="pl-3 py-3">{setting.title}</td>
-                        <td className="pl-3 py-3"   >  
-                            <Image   src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/${setting.logo}`} unoptimized={true} alt="image" className="w-12 h-12" width={'100'} height={'100'} />
-                        </td>
-                         <td className="pl-3 py-3"   > 
-                              <Image   src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/${setting.icon}`} unoptimized={true} alt="image" className="w-12 h-12" width={'100'} height={'100'} />
-   </td>
-                        <td className="pl-3 py-3">{setting.keywords}</td>
-                       
-                        <td className="pl-3 py-3">{setting.description.replace(/<(.|\n)*?>/g, '').slice(0, 10)}</td>
-                        
-                        <td>
-                            <Link href={`${pathname}/edit/${setting.id}`} className="py-2 px-4 bg-green-500 hover:bg-green-600  rounded text-white">  <FontAwesomeIcon icon={faEdit} />     </Link>
-                             
-                        </td>
-                    </tr>)
-            })}
-                </tbody>    
-             </Table>}
-        </TableContainer>
-    </>
+    return (
+     <CustomTable 
+        title={'تنظیمات'}
+        sitemap={'  تنظیمات'}
+        pagination={settings?.meta}
+        data={query}
+        columns={columns} />
     )
 }
 export default Index;
